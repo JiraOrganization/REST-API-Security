70aa416b18a6daba38b58ac75871dc108a62101f
sangtae <devsangtae@gmail.com>
2022-07-12 22:53:18 +0900

    [RAS-8] [Bad Request 처리]

--- src/test/java/com/example/spingbootrest/events/EventControllerTest.java
+++ src/test/java/com/example/spingbootrest/events/EventControllerTest.java
@@ -41,7 +41,7 @@ public class EventControllerTest {
      * HTTP Status code 201로 성공하는지 확인
      * @throws Exception JackMapper 예외
      */
-    @Test
+    @Test @DisplayName("정상적인 이벤트가 생성되는 테스트")
     void createEvent() throws Exception {
 
         EventDto eventDto = EventDto.builder()
@@ -71,6 +71,54 @@ public class EventControllerTest {
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
 
 
+    }
+
+    @Test @DisplayName("입력값이 잘못된 이벤트 생성 경우의 테스트")
+    void createEvent_Bad_Request_Wrong_Input() throws Exception {
+
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11,25,14,21 ))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 24,14,21))
+                .endEventDateTime(LocalDateTime.of(2018, 11,23,14,21))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(eventDto)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].field").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].rejectedValue").exists())
+        ;
+
+    }
+
+
+    @Test @DisplayName("입력 값이 없는 이벤트 생성 테스트")
+    public void ctrateEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto event = EventDto.builder().build();
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test @DisplayName("입력 받을수 없는 데이터로 이벤트 생성하는 경우 테스트")
+    public void createEvent_Bad_Request_Input() throws Exception {
         Event event = Event.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
@@ -92,12 +140,8 @@ public class EventControllerTest {
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isBadRequest())
-
-
-        ;
+                .andExpect(status().isBadRequest());
     }
-
 
 
 }
